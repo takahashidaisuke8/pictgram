c4cdecb2d18a2a7dd6d8b00ba4f746cf8bb69e79
takahashi daisuke <adiskp17@gmail.com>
2020-09-29 12:10:05 +0000

    form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,3 +6,20 @@ class UsersController < ApplicationController
   def create
   end
 end
+
+def create
+  @user = User.new(name: params[:user][:name], email: params[:user][:email])
+  if @user.save
+    redirect_to root_path, success: '登録が完了しました'
+  else
+    flash.now[:danger] = "登録に失敗しました"
+    render :new
+  end
+end
+  
+private
+def user_params
+  params.require(:user).permit(:name, :email)
+end
+end
+
