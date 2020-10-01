d65ef2a780d8f770e74209e3ed7b57b4c62595d0
takahashi daisuke <adiskp17@gmail.com>
2020-10-01 11:45:05 +0000

    ?????

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, length: { maximum: 15 }
+    validates :email, format: { with: /\A[a-z0-9]+@[a-z0-9]+.[a-z]+\z/i }
+    validates :password, length: { minimum: 8, maximum: 32 }, format: { with: /\A[A-Za-z0-9]+\z/}
+    
     
     has_secure_password
 end
